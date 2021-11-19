7d1220774a4ebcdd427011b6b42db66f5a54c011
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-11-19 15:52:17 +0530

    multipe players

--- DeckOfCard.java
+++ DeckOfCard.java
@@ -1,24 +1,47 @@
 package com.bridgelabz;
 
-public class DeckOfCard {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-	
-	public static void uniqueCards() {
-		
-		String[] deck = new String[52];
-	    String[] suits = { "Diamonds", "Clubs","Spades", "Hearts",};
-	    String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King","Ace"};
-	    
-	    int count = 0;
+public class DeckOfCards {
+    static ArrayList<Player> players = new ArrayList<>();
+    String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    String[] deck = new String[52];
+
+    
+    public void uniqueDeckOfCard() {
+        int count = 0;
         for (int row = 0; row < suits.length; row++) {
             for (int column = 0; column < ranks.length; column++) {
                 deck[count] = ranks[column] + "-" + suits[row] + " ";
                 count++;
             }
-           }
         }
-        public static void main(String[] args) {
-            DeckOfCard cards = new DeckOfCard();
-            cards.uniqueCards();
+
+    }
+
+    
+    public void addPlayers() {
+        int numberOfPlayers;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of players");
+        numberOfPlayers = sc.nextInt();
+        if (numberOfPlayers < 2 || numberOfPlayers > 4) {
+            System.out.println("Enter the value between 2 and 4");
+            numberOfPlayers = sc.nextInt();
+        }
+        for (int i = 1; i <= numberOfPlayers; i++) {
+            System.out.println("Enter the players name");
+            Player player = new Player(sc.next());
+            players.add(player);
+
         }
     }
+
+    public static void main(String[] args) {
+        DeckOfCards cards = new DeckOfCards();
+        cards.uniqueDeckOfCard();
+        cards.addPlayers();
+    }
+}
---
+++ Player.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Player {
+    public Player(String firstname) {
+        this.firstname = firstname;
+    }
+
+    String firstname;
+}
